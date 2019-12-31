8e0d2194c2b9264b2d9958ee87c833c50140de94
Eitaro Fukamachi <e.arrows@gmail.com>
2019-12-31 16:48:36 +0900

    v0.9.1

--- getac.rb
+++ getac.rb
@@ -1,8 +1,8 @@
 class Getac < Formula
   desc "Quick unit testing tool for competitive programming"
   homepage "https://github.com/fukamachi/getac"
-  url "https://github.com/fukamachi/getac.git", :tag => "0.9.0"
-  sha256 "ea06b255adf6e162c37ff23b0e1c897ce4a457e8845c34c4ef73f23b9902cccd"
+  url "https://github.com/fukamachi/getac.git", :tag => "0.9.1"
+  sha256 "0e08fb2acfaf67b036b23f52cbe7f000fe3c19ce496784901aac95f459049158"
   head "https://github.com/fukamachi/getac.git", :branch => "master"
 
   depends_on "sbcl"
